dd81b86d28e31803e648b1487513bf4f0a3bbb88
adamsEugene <adamseugene292@gmail.com>
2024-04-26 00:08:23 +0000

    done with assignment 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
 import { useState } from "react";
 import "./App.css";
 
+const _activities = [
+  { title: "go", status: "pending" },
+  { title: "come", status: "in-progress" },
+  { title: "sit", status: "in-progress" },
+  { title: "eat", status: "pending" },
+  { title: "dance", status: "completed" },
+  { title: "move", status: "completed" },
+];
+
 function App() {
-  const [state, setState] = useState(0);
-  const [updatedValue, setUpdatedValue] = useState(0);
+  const [value, setValue] = useState("");
+  const [activities, setActivities] = useState(_activities);
 
-  const increase = () => {
-    setState((prev) => prev + 1);
-  };
-
-  function decrease() {
-    setState(function (prev) {
-      return prev - 1;
-    });
-  }
-
-  const unUpdated = () => {
-    setState(updatedValue);
+  const onSubmit = () => {
+    setActivities((prev) => [{ title: value, status: "pending" }, ...prev]);
+    setValue("");
   };
 
   return (
     <div className="main_wrapper">
       <div className="wrapper">
-        <h1 className="value">{state}</h1>
-        <div className="buttons_wrapper">
-          <button onClick={decrease}>decrease</button>
+        <h1>Adams Activities</h1>
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.title}>
+              <label htmlFor={activity.title}>{activity.title}</label>
+              <input
+                type="checkbox"
+                name="check"
+                checked={activity.status === "completed"}
+                id={activity.title}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="inputs">
+          <label htmlFor="user_input">Enter an activity</label>
           <input
-            type="number"
-            name="adams"
-            id="adams"
-            onChange={(e) => setUpdatedValue(+e.target.value)}
+            type="text"
+            name="user_input"
+            id="user_input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
           />
-          <button onClick={increase}>increase</button>
+          <button onClick={onSubmit}>submit</button>
         </div>
-        <button onClick={unUpdated}>update</button>
       </div>
     </div>
   );
